fbf48caf4cdadc4085332e089dadcde2446588bd
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-03-05 11:20:47 +0100

    Implement vault controller

---
+++ src/main/java/czertainly/common/credential/provider/api/VaultControllerImpl.java
@@ -0,0 +1,22 @@
+package czertainly.common.credential.provider.api;
+
+import com.czertainly.api.exception.NotFoundException;
+import com.czertainly.api.interfaces.connector.secrets.VaultController;
+import com.czertainly.api.model.client.attribute.RequestAttribute;
+import com.czertainly.api.model.common.attribute.common.BaseAttribute;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class VaultControllerImpl implements VaultController {
+    @Override
+    public void checkVaultConnection(List<RequestAttribute> attributes) throws NotFoundException {
+
+    }
+
+    @Override
+    public List<BaseAttribute> listVaultAttributes() throws NotFoundException {
+        return List.of();
+    }
+}
